eadaea9c9f11dd7c822938bba454b46369cf0f63
ßrunys <brunysixl@gmail.com>
2024-06-22 10:55:31 -0400

    Update smoothscroll.js

--- assets/js/smoothscroll.js
+++ assets/js/smoothscroll.js
@@ -1,7 +1,6 @@
         document.querySelectorAll('a[href^="#"]').forEach(anchor => {
             anchor.addEventListener('click', function(e) {
                 e.preventDefault();
-
                 const target = document.querySelector(this.getAttribute('href'));
                 if (target) {
                     target.scrollIntoView({
@@ -10,3 +9,43 @@
                 }
             });
         });
+
+
+        function loadTheme() {
+            const savedTheme = localStorage.getItem('theme');
+            if (savedTheme) {
+                setTheme(savedTheme, false);
+            }
+        }
+
+
+        function setTheme(theme, save = true) {
+            switch (theme) {
+                case 'theme-ocean':
+                    document.documentElement.style.setProperty('--navbar-color', '#1b0c63');
+                    document.documentElement.style.setProperty('--background-color', '#412ea3');
+                    document.documentElement.style.setProperty('--cover-color', '#412ea3');
+                    document.documentElement.style.setProperty('--content-background', '#7e6bdb');
+                    document.documentElement.style.setProperty('--footer-background', '#1b0c63');
+                    break;
+                case 'theme-test':
+                    document.documentElement.style.setProperty('--navbar-color', '#333333');
+                    document.documentElement.style.setProperty('--background-color', '#1a1a1a');
+                    document.documentElement.style.setProperty('--cover-color', '#000000');
+                    document.documentElement.style.setProperty('--content-background', '#1a1a1a');
+                    document.documentElement.style.setProperty('--footer-background', '#333333');
+                    break;
+                default:
+                    document.documentElement.style.setProperty('--navbar-color', '#0d0d0d');
+                    document.documentElement.style.setProperty('--background-color', '#0d0d0d');
+                    document.documentElement.style.setProperty('--cover-color', '#0d0d0d');
+                    document.documentElement.style.setProperty('--content-background', '#0d0d0d');
+                    document.documentElement.style.setProperty('--footer-background', '#0d0d0d');
+            }
+            if (save) {
+                localStorage.setItem('theme', theme);
+            }
+        }
+
+        // Apply the theme on load
+        window.onload = loadTheme;
