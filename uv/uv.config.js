c827b472b45eb0dea175c1155fed9f5bde816930
ßrunys <brunysixl@gmail.com>
2024-06-29 00:41:29 -0400

    frogies arcade on top

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -2,7 +2,7 @@ self.__uv$config = {
   prefix: "/service/",
 
   /* Bare server URL */
-  bare: "https://tomp.app",
+  bare: "http://45.90.12.122:7161",
 
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
