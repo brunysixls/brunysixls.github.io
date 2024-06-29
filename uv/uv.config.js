05d327646f7c8bd73ad2eced71cb4f6179dc176b
ßrunys <brunysixl@gmail.com>
2024-06-29 02:51:50 -0400

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -2,7 +2,7 @@ self.__uv$config = {
   prefix: "/service/",
 
   /* Bare server URL */
-  bare: "https://https://superfat.yourmom.eu.org",
+  bare: "https://superfat.yourmom.eu.org",
 
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
